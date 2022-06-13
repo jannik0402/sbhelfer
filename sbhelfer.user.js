43e4360420d355539c9d94937af9b60d3645f3fb
jannik0402 <78054507+jannik0402@users.noreply.github.com>
2022-06-13 15:28:12 +0200

    Update sbhelfer.user.js

--- sbhelfer.user.js
+++ sbhelfer.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         SB-Helfertool
 // @namespace	 https://github.com/jannik0402/sbhelfer
-// @version      1.5.0
+// @version      1.5.1
 // @description  Kleines Tool für die Arbeit des Schwarzwälder Boten. Beim öffnen eines Bildes öffnet sich automatisch die Original-URL (ohne 1024 oder 700). Beim Klick von "L" auf der Seite, wird der relative Link in die Zwischenablage kopiert.
 // @author       Jannik Nölke
 // @match        https://www.schwarzwaelder-bote.de/*
@@ -77,8 +77,6 @@
                 alert("\n\nDu bist ein echter Spaßvogel! 🤣🤣🤣 \n\nEin relativer Link der Startseite macht doch nun wirklich keinen Sinn...\n\n");
         	}}
     
-    
-    }
 	
 	if(evt.keyCode == '51'){
 
@@ -90,7 +88,6 @@
 		}
 		
         	if(neuerlink != "/"){
-            		alert(neuerlink + "\n\nIn die Zwischenablage kopiert! ✅");
 			window.open('https://keen-albattani.85-215-230-13.plesk.page/generatoren/ausgabe/design2/tarue.php?link=' + neuerlink);
 			
         	}else{
